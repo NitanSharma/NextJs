3a7d78fe5dbf148dbc0de512c2438f2d051edce2
NitanSharma <sharmanitan938@gmail.com>
2025-09-04 15:32:09 +0530

    Some Nextjs concept

--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,10 +1,17 @@
+import Link from "next/link"
 export default function ProductList(){
+    const productId = 100;
     return (
         <>
+        <Link href="/">Home</Link>
         <h1>Product List</h1>
         <h2>Product 1</h2>
         <h2>Product 2</h2>
         <h2>Product 3</h2>
+        <h2>
+            <Link href={`/products/${productId}`} replace>Product {productId}</Link>
+            {/* this replace will go to straight Home page */}
+        </h2>
         </>
     )
 }
